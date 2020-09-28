9352d394156b8064e9f0a1d09855652da75a3dc9
ttack-hub0706 <tkm_fjhr0706@icloud.com>
2020-09-28 17:05:24 +0900

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <div class="center">
+      <h1>Weather App</h1>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header{
+  height: 70px;
+  line-height: 70px;
+  background-color: rgba(0,0,0,1);
+}
+
+.center{
+  font-size: 20px;
+  font-weight: bold;
+  color: #fff;
+  text-align: center;
+}
+</style>
